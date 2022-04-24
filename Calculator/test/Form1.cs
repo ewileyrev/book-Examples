78e419bf89d204dba050dbd682b7b6c91693bd0c
Ethan <ewiley2018@outlook.com>
2022-04-24 07:29:48 -0400

    adding

---
+++ Calculator/test/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Int32 num1 = Convert.ToInt32(textBox1.Text);
+            Int32 num2 = Convert.ToInt32(textBox2.Text);
+            Int32 result = num1 - num2;
+            label2.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Int32 num1 = Convert.ToInt32(textBox1.Text);
+            Int32 num2 = Convert.ToInt32(textBox2.Text);
+            Int32 result = num1 + num2;
+            label2.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Int32 num1 = Convert.ToInt32(textBox1.Text);
+            Int32 num2 = Convert.ToInt32(textBox2.Text);
+            Int32 result = num1 * num2;
+            label2.Text = result.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Int32 num1 = Convert.ToInt32(textBox1.Text);
+            Int32 num2 = Convert.ToInt32(textBox2.Text);
+            Int32 result = num1 / num2;
+            label2.Text = result.ToString();
+        }
+    }
+}
---
+++ JohnDoesBrowser/JohnDoesBrowser/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace JohnDoesBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.Url = new System.Windows.Forms.TextBox();
+            this.Go = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(1, 39);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(704, 537);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // Url
+            // 
+            this.Url.Location = new System.Drawing.Point(13, 13);
+            this.Url.Name = "Url";
+            this.Url.Size = new System.Drawing.Size(584, 20);
+            this.Url.TabIndex = 1;
+            this.Url.Text = "http://";
+            // 
+            // Go
+            // 
+            this.Go.Location = new System.Drawing.Point(604, 13);
+            this.Go.Name = "Go";
+            this.Go.Size = new System.Drawing.Size(75, 23);
+            this.Go.TabIndex = 2;
+            this.Go.Text = "Go";
+            this.Go.UseVisualStyleBackColor = true;
+            this.Go.Click += new System.EventHandler(this.Go_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 601);
+            this.Controls.Add(this.Go);
+            this.Controls.Add(this.Url);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "John Doe\'s Browser";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox Url;
+        private System.Windows.Forms.Button Go;
+    }
+}
+
